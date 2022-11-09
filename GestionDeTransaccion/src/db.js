091cd4bba6ee2e8f37460a1036ba6d29d92f0939
javierIba <82604865+javierIba@users.noreply.github.com>
2022-11-09 02:29:24 -0300

    Update db.js

--- GestionDeTransaccion/src/db.js
+++ GestionDeTransaccion/src/db.js
@@ -38,18 +38,3 @@ pool.query = promisify(pool.query);
 module.exports=pool;
 
 
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : '22.jaigibac.22'
-// });
- 
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });
